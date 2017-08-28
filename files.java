fbaf73a1d26126e757e67e3e85918de4a3a2b488
jghoover <30665181+jghoover@users.noreply.github.com>
2017-08-28 09:59:34 -0500

    added comments

--- files.java
+++ files.java
@@ -8,29 +8,33 @@ public class files
 
 	public static void main(String[] args) throws IOException
 	{
-		int n = 10000;
-		int [] a = new int[n];
+		int n = 10000; //avoid "magic numbers". use a variable where you would repeat constants in loops, etc.
 		
+		int [] a = new int[n]; //declare an array of length n
+		
+		//populate the array using a for loop
 		for(int i = 0; i<n; i++)
 		{
 			a[i]=(int)(1+100*Math.random());
 			//System.out.println(a[i]);
 		}
 		
+		//write the elements of the array to a text file
 		FileWriter fout = new FileWriter("textfile.txt");
 		
 		for(int i = 0; i<n; i++)
 		{
+			//use the String.valueOf() function to avoid the writer trying to interperate the int values as characters
 			fout.write((String.valueOf(a[i]))+'\n');
 		}
 		
-		fout.flush();
+		fout.flush(); //make sure everything in the filewriter has actually been written
 		
 		fout.close();
 		
 		Scanner fin = new Scanner(new FileReader("textfile.txt"));
 		
-		do
+		do //read in each line of the text file and print it
 		{
 			System.out.println(fin.nextLine());
 		}
